6f5c6e49294285c5ba84210eeec2cd8538143a72
taha-sindi <taha.sindi@gmail.com>
2019-05-20 11:50:02 +0300

    update

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -69,6 +69,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params.require(:author).permit(:first_name, :last_name, :location)
+      params.require(:author).permit(:first_name, :last_name, :location, :nickname)
     end
 end
